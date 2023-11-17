2daaadb3056f362b2f644e6233dcd6eee113f395
Tushar12152 <tusharimran470@gmail.com>
2023-11-17 19:09:16 +0600

    specific data get from menu collection

--- index.js
+++ index.js
@@ -167,6 +167,15 @@ const verifyAdmin=async(req,res,next)=>{
           res.send(result)
     })
 
+
+    app.get('/menu/:id',async(req,res)=>{
+           const id=req.params.id;
+           const  query={_id:new ObjectId(id)}
+           const result= await menuCollection.findOne(query)
+           res.send(result)
+    })
+
+
     app.delete('/menu/:id',verifyToken,async(req,res)=>{
              const id=req.params.id;
              const query={_id:new ObjectId(id)}
